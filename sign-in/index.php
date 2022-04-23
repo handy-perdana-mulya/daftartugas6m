fa51fccd8619df9382cf638b740e72bab29506b4
Handy <handyperdana8@gmail.com>
2022-04-23 14:38:59 +0800

    mengarahkan ke dashboard

--- sign-in/index.php
+++ sign-in/index.php
@@ -57,6 +57,7 @@
       $row_count = $statement->rowCount();
   
       if ($row_count > 0){
+        header('Location: ../dashboard/index.php')
       ?>
       <div class="alert alert-success" role="alert">
       Login Berhasil
